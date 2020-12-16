d4c369352e9de1aac43c39d1be35ec448ea2640d
skeep194 <skeep194@knu.ac.kr>
2020-12-17 01:15:39 +0900

    server url updated

--- kickboard-back/index.js
+++ kickboard-back/index.js
@@ -26,6 +26,6 @@ app.use('/auth', authRouter)
 app.use('/kickboard', kickboardRouter)
 app.use('/user', userRouter)
 
-const server = app.listen(4444, () => {
+const server = app.listen(3000, () => {
     console.log('start')
 })
