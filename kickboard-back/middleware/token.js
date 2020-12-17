21f848eaa4e8714c078be56201c19326e2005f30
skeep194 <skeep194@knu.ac.kr>
2020-12-17 20:23:34 +0900

    user page name add

--- kickboard-back/middleware/token.js
+++ kickboard-back/middleware/token.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 function verifyToken(req, res, next) {
     try {
-        req.decoded = jwt.verify(req.query.authorization, global.JWT_SECRET)
+        req.decoded = jwt.verify(req.headers.authorization, global.JWT_SECRET)
         return next()
     }
     catch (error) {
